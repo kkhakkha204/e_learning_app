a13607b3879b168482b331d629439b5762bc9204
khacl <khaclaude1@gmail.com>
2026-01-08 11:20:06 +0700

    part13

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -1,3 +1,7 @@
+import 'package:e_learning_app/core/theme/app_colors.dart';
+import 'package:e_learning_app/views/profile/widgets/profile_app_bar.dart';
+import 'package:e_learning_app/views/profile/widgets/profile_options.dart';
+import 'package:e_learning_app/views/profile/widgets/profile_stats_card.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,9 +9,29 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile Screen'),
+    return const Scaffold(
+      backgroundColor: AppColors.lightBackground,
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          ProfileAppBar(
+            initials: 'TK',
+            fullName: 'Tuan Kha',
+            email: 'kha@gmail.com',
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  ProfileStatsCard(),
+                  SizedBox(height: 24),
+                  ProfileOptions(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
